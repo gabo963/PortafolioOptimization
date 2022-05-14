ad46476b854355374da9e3fe2f6e7f384e490f12
gabo963 <gsarmiento101874@gmail.com>
2022-05-13 21:39:42 -0500

    Initial Commit

---
+++ src/Interfaz/PanelInfo.java
@@ -0,0 +1,44 @@
+package Interfaz;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class PanelInfo extends JPanel implements ActionListener 
+{
+
+	public final static String CARGAR = "Cargar Portafolios";
+
+	private static final long serialVersionUID = 1L;
+
+	private InterfazOptimizador principal;
+
+	private JButton btnCargar;
+
+	public PanelInfo( InterfazOptimizador pPrincipal )
+	{
+		setLayout( new GridLayout( 1,1) );
+
+		principal = pPrincipal;
+
+		btnCargar = new JButton( CARGAR );
+		btnCargar.setActionCommand( CARGAR );
+		btnCargar.addActionListener( this );
+		add(btnCargar);
+		
+
+	}
+
+	public void actionPerformed(ActionEvent f) 
+	{
+		if( f.getActionCommand() == CARGAR )
+		{
+			principal.cargarArchivo();
+		}
+
+	}
+
+}
